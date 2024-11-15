99eb58ecc22dc9732261a2995cbe2b90149864e0
vicenttcarvalho <vicentecleyton@gmail.com>
2024-11-15 16:11:19 -0300

    ORM

--- dslist/src/main/java/com/devsuperior/dslist/entities/Game.java
+++ dslist/src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -1,10 +1,20 @@
 package com.devsuperior.dslist.entities;
 
+import jakarta.persistence.*;
+
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_game")
+
 public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+
+    //Mudar o nome da coluna year, pq existe a palavra year reservada no sql para year
+    @Column(name = "game_year")
     private Integer year;
     private String genre;
     private String platforms;
