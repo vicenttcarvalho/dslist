c61307b9c609d508b4e72a4755e4e859eeaceb65
vicenttcarvalho <vicentecleyton@gmail.com>
2024-11-16 16:44:08 -0300

    Consulta SQL, projection, busca de games por lista

--- dslist/src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ dslist/src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -2,6 +2,7 @@ package com.devsuperior.dslist.dto;
 
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
     private Long id;
@@ -22,6 +23,13 @@ public class GameMinDTO {
         shortDescription = entity.getShortDescription();
     }
 
+    public GameMinDTO(GameMinProjection projection) {
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
+    }
     public Long getId() {
         return id;
     }
